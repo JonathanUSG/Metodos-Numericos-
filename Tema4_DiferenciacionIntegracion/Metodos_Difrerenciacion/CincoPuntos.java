d7ba2e35cfedc3b19e7d553f815e40fbba803dda
JonathanUSG <23030011@itesa.edu.mx>
2025-04-18 16:22:30 +0000

    Commit 2: Se agregó el método cincoPuntos, el cual implementa las fórmulas del método, y se imprime el resultado final.

--- Tema4_DiferenciacionIntegracion/Metodos_Difrerenciacion/CincoPuntos.java
+++ Tema4_DiferenciacionIntegracion/Metodos_Difrerenciacion/CincoPuntos.java
@@ -6,13 +6,26 @@ public class CincoPuntos {
     double x = 1;
     double h = 0.1;
     int n = 4;
-    
+
+    // Calcular la derivada usando el método de cinco puntos
+    double resultado = cincoPuntos(x, h);
+
     //Imprimir Información
     System.out.println("Función: x * sin(x)");
     System.out.println("Punto de Derivación: " + x);
-
+    System.out.printf("Resultado de la Derivada por el método de los Cinco Puntos: %.4g%n", resultado);
     }
     public static double funcion(double x){
         return x * Math.sin(x);
+    }
+    public static double cincoPuntos(double x, double h){
+        //Evaluar la función en los puntos necesarios
+        double fx1 = funcion(x + 2 * h); // f(x + 2h)
+        double fx2 = funcion(x + h);     // f(x + h)
+        double fx3 = funcion(x - h);     // f(x - h)
+        double fx4 = funcion(x - 2 * h); // f(x - 2h)
+
+        //Aplicar la fórmula de los Cinco Puntos: [- f(x + 2h) + 8f(x + h) - 8f(x - h) + f(x - 2h)] / (12h)
+        return (-fx1 + 8 * fx2 - 8 * fx3 + fx4) / (12 * h);
     }
 }
