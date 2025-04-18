4de54bc632cb5b981f63bc3437a13246a0c54ba9
JonathanUSG <23030011@itesa.edu.mx>
2025-04-18 15:24:22 +0000

    Commit 1: Se definieron los valores de x, h y n, además de que se creó un método en donde se podrá colocar la función que se deseé.

---
+++ Tema4_DiferenciacionIntegracion/Metodos_Difrerenciacion/TresPuntos.java
@@ -0,0 +1,20 @@
+package Tema4_DiferenciacionIntegracion.Metodos_Difrerenciacion;
+
+public class TresPuntos {
+    public static void main(String[] args) {
+        double x = 1; // Punto donde se calcula la derivada.
+        double h = 0.1; // Tamaño del Paso
+        int n = 4;  // Número Fijo para compatibilidad 
+
+        //Imprimir Información de la Función
+        System.out.println("Función: x * sin(x)");
+        System.out.println("Punto de derivación: " + x);
+        System.out.println("Tamaño del Paso: " + h);
+    }
+
+    // Definir la función a derivar, puede modificarse
+    public static double funcion(double x){
+        return x * Math.sin(x); // Función f(x) = x * sin(x)
+    }
+
+}
