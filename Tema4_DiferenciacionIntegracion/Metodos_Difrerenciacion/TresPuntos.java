deef5fa219aa1171970175e5fade78375fc3122f
JonathanUSG <23030011@itesa.edu.mx>
2025-04-18 16:06:00 +0000

    Commit 2: Se creó el método tresPuntos, el cual aplica la fórmula del método, además, se le aplicaron comentarios al código para hacerlo más claro.

--- Tema4_DiferenciacionIntegracion/Metodos_Difrerenciacion/TresPuntos.java
+++ Tema4_DiferenciacionIntegracion/Metodos_Difrerenciacion/TresPuntos.java
@@ -6,10 +6,14 @@ public class TresPuntos {
         double h = 0.1; // Tamaño del Paso
         int n = 4;  // Número Fijo para compatibilidad 
 
+        // Calcular la derivada usando el método de tres puntos
+        double resultado = tresPuntos(x, h);
+
         //Imprimir Información de la Función 
-        System.out.println("Función: x * sin(x)");
-        System.out.println("Punto de derivación: " + x);
-        System.out.println("Tamaño del Paso: " + h);
+        System.out.println("Función: x * sin(x)");  // Función Evaluada
+        System.out.println("Punto de derivación: " + x); // Punto de Evaluación
+        System.out.println("Tamaño del Paso: " + h);    // Tamaño del Paso
+        System.out.printf("Resultado de la derivada por el método de los Tres puntos: %.3g%n", resultado);
     }
 
     // Definir la función a derivar, puede modificarse
@@ -17,4 +21,12 @@ public class TresPuntos {
         return x * Math.sin(x); // Función f(x) = x * sin(x)
     }
 
+    // Calcula la derivada numérica usando la fórmula de tres puntos
+    public static double tresPuntos(double x, double h){
+        double fx1 = funcion(x + + h); // f(x + h)
+        double fx2 = funcion(x - h);    // f(x - h)
+
+        //Aplicando la fórmula: [f(x + h) - f(x - h)] / (2h)
+        return (fx1 - fx2) / (2 * h);
+    }
 }
