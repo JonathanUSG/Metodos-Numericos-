e1e5ec0af67611eb4b9818a16515a4caf2853b94
JonathanUSG <23030011@itesa.edu.mx>
2025-04-19 17:03:01 +0000

    Commit 2: Se implementó el método trapecio para encontrar la Integral de la función a integrar usando las fórmulas del método.

--- Tema4_DiferenciacionIntegracion/Metodos_Integracion/Trapecio.java
+++ Tema4_DiferenciacionIntegracion/Metodos_Integracion/Trapecio.java
@@ -5,19 +5,36 @@ public class Trapecio {
          //Definir Parámetros fijos para la Integración
          double a = 0;      // Límite Inferior
          double b = 2;      // Límite Superior
-         int n = 4;         // Número deSegmentos
+         int n = 4;         // Número de Segmentos
+
+         // Calcular La Integral usando la Regla del Trapecio
+         double resultado = trapecio(a, b, n);
 
         // Imprimir Información y resultado Formateado
         System.out.println("Función: x * sin(x)");
         System.out.println("Límite Inferior: " + (int) a);
         System.out.println("Límite Superior: " + (int) b);
         System.out.println("Número de Segmentos: " + n);
-
+        System.out.printf("Resultado de la integral por el método del Trapecio: %.4g%n", resultado);
     }
 
     // Método que define la función a integrar, es modificable
-    public static double función(double x){
+    public static double funcion(double x){
         return x * Math.sin(x);
     }  
-    
+
+    public static double trapecio(double a, double b, int n){
+        //Calcular el tamaño del Paso
+        double h = (b - a) / n;
+
+        //Sumar los extremos: (f(a) + f(b)) / 2
+        double suma = (funcion(a) + funcion(b)) / 2;
+
+        // Sumar Los Puntos Intermedios: f(x_i) para i = 1  hasta n - 1
+        for(int i = 0; i < n; i++ ){
+            double x = a + i * h;
+            suma += funcion(x);
+        }
+        return h * suma;
+    }
 }
