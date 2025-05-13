53ad8db0d5e7dc607c587813a4085790c9bd4627
JonathanUSG <23030011@itesa.edu.mx>
2025-05-13 14:38:11 +0000

    Commit 2: Se implementó el método main, en donde se planteó el ejercicio, para posteriormente mostrar el resultado del ejercicio, así como los errores (absoluto y porcentual).

--- Tema5_Interpolacion/Metodos_Analisis_Datos/Correlacion.java
+++ Tema5_Interpolacion/Metodos_Analisis_Datos/Correlacion.java
@@ -2,7 +2,40 @@ package Tema5_Interpolacion.Metodos_Analisis_Datos;
 
 public class Correlacion {
     public static void main(String[] args) {
-        
+        // Puntos conocidos en x, y
+        double [] x = {1, 2, 3, 4, 5};
+        double [] y = {2, 4, 6, 8, 10};
+
+        //Mostrar los puntos utilizados
+        System.out.println("Puntos Utilizados:");
+        for(int i = 0; i < x.length; i++){
+            System.out.printf("x = %.1f, y = %.1f\n", x[i], y[i]);
+        }
+
+        // Calcular el coeficiente de correlación
+        double r = calcularCorrelacion(x,y);
+
+        // Mostrar el resultado del coeficiente
+        System.out.printf("\nCoeficiente de correlación: %.4f\n", r);
+
+        // Calcular y mostrar el error absoluto y porcentual
+        double errorAbsoluto = calcularErrorAbsoluto(r);
+        double errorPorcentual = calcularErrorPorcentual(r);
+
+        System.out.printf("Error Absoluto: %.4f\n", errorAbsoluto);
+        System.out.printf("Error Porcentual: %.2f%%\n", errorPorcentual);
+
+        // Interpretación rápida
+        if (r == 1) 
+            System.out.println("Correlación Perfecta Positiva");
+         else if(r == -1)
+            System.out.println("Correlación Perfecta Negativa");
+         else if(r > 0)
+            System.out.println("Correlación Positiva");
+         else if(r < 0)
+            System.out.println("Correlación Negativa");
+         else 
+            System.out.println("Sin correlación");
     }
 
     // Método para calcular el coeficiente de correlación de Pearson
