4f29e02f4d68294b5c56e6c49971b428621cf8cd
JonathanUSG <23030011@itesa.edu.mx>
2025-05-25 21:03:53 +0000

    Commit 1: Se creó la clase básica MetodoSecante con la función f(x) que define la ecuación para la cual se buscará la raíz.

---
+++ Tema2_SolucionEcuaciones/Metodos_Abiertos/MetodoSecante.java
@@ -0,0 +1,11 @@
+package Tema2_SolucionEcuaciones.Metodos_Abiertos;
+
+import java.util.Scanner;
+
+public class MetodoSecante {
+    // Esta es la función f(x) que se quiere igualar a 0
+    // Puedes cambiarla según el problema que quieras resolver
+    public static double f(double x) {
+        return Math.pow(x, 3) - x - 2;  // En este ejemplo: f(x) = x^3 - x - 2
+    }
+}
