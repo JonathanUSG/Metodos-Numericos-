b4cf8f82502702ac1c1f465c9a244a2f063c4001
JonathanUSG <23030011@itesa.edu.mx>
2025-05-27 21:11:42 +0000

    Commit 2: Este commit agrega el bucle de Runge-Kutta para calcular los primeros 3 puntos necesarios para el método de Adams-Bashforth. También, se añade la funcion f, la cual obtiene la función f(x, y) que define la ecuación diferencial

--- Tema6_Ecuaciones_Diferenciales/Metodos_Pasos_Multiples/AdamsBashforth.java
+++ Tema6_Ecuaciones_Diferenciales/Metodos_Pasos_Multiples/AdamsBashforth.java
@@ -36,5 +36,22 @@ public class AdamsBashforth {
         x[0] = x0;
         y[0] = y0;
         f[0] = f(x[0], y[0]);
+
+        // Usamos Runge-Kutta para calcular los primeros 3 puntos adicionales
+        for (int i = 0; i < 3; i++) {
+            double k1 = f(x[i], y[i]);
+            double k2 = f(x[i] + h/2, y[i] + (h * k1)/2);
+            double k3 = f(x[i] + h/2, y[i] + (h * k2)/2);
+            double k4 = f(x[i] + h, y[i] + h * k3);
+
+            x[i + 1] = x[i] + h;
+            y[i + 1] = y[i] + (h/6) * (k1 + 2*k2 + 2*k3 + k4);
+            f[i + 1] = f(x[i + 1], y[i + 1]);
+        }
+    }
+    // Función que define la ecuación diferencial dy/dx = f(x, y)
+    // En este ejemplo, usamos dy/dx = -2x*y (puedes cambiarla según necesites)
+    public static double f(double x, double y) {
+        return -2 * x * y;
     }
 }
